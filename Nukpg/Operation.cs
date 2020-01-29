9f1b9ed90ccd6a9e8351bf02236ba2cc6546d987
Yevhen K <yevhenk@varyence.com>
2020-01-29 19:19:58 +0200

    added some logic

---
+++ Nukpg/Operation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Nukpg
+{
+    public static class Operation
+    {
+        public static int Add(int number) =>
+            ++number;
+
+        public static int Subtract(int number) =>
+            --number;
+    }
+}
